39b06449d4ad8b190e37d4b1d02f579cfeecff47
Magda Łuszczewska <magdaluslodka@gmail.com>
2018-02-08 14:41:13 +0100

    Add js file

---
+++ script.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name);//alert will display your name in browser
+console.log('Hello, ' + name);
+
+/* this line will be not displayed
+console.log
+*/
+//above I used two types of comments
